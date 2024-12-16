7c34171fd78085a4585f97c21565837ea45ecaca
Jilan-Hisham-Khashaba <154570411+Jilan-Hisham-Khashaba@users.noreply.github.com>
2024-12-16 04:08:10 +0200

    Add files via upload

---
+++ Back end/Gp.Api solution/AdminDashBoard/Helpers/UserImageResolver.cs
@@ -0,0 +1,28 @@
+﻿using AdminDashBoard.Models;
+using AutoMapper;
+using Gp.Api.Dtos;
+using GP.core.Entities.identity;
+
+namespace AdminDashBoard.Helpers
+{
+    public class UserImageResolver : IValueResolver<AppUser, UserView, string>
+    {
+        private readonly IConfiguration configuration;
+
+        public UserImageResolver(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+        public string Resolve(AppUser source, UserView destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PhotoPicture))
+            {
+                return $"{configuration["ApiBaseUrl"]}images/PictureAdmin/{source.PhotoPicture}";
+            }
+
+            return string.Empty;
+        }
+    }
+}
