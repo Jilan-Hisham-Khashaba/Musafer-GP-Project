83a38ebbc5c9a37a5b92c0dd0acc1532e8d2e725
Jilan-Hisham-Khashaba <154570411+Jilan-Hisham-Khashaba@users.noreply.github.com>
2024-12-16 04:12:01 +0200

    Add files via upload

---
+++ Back end/Gp.Api solution/AdminDashBoard/Profiles/MappingProfileee.cs
@@ -0,0 +1,29 @@
+﻿using AdminDashBoard.Models;
+using AutoMapper;
+using GP.Core.Entities;
+
+namespace AdminDashBoard.Profiles
+{
+    public class MappingProfileee : Profile
+    {
+        public MappingProfileee()
+        {
+            CreateMap<CategoryModel, Category>().ForMember(pd => pd.TypeName, o => o.MapFrom(T => T.Name))
+                .ReverseMap();
+
+            CreateMap<CitiesModel, City>()
+                .ForMember(pd => pd.Id, o => o.MapFrom(T => T.Id))
+                 .ForMember(pd => pd.NameOfCity, o => o.MapFrom(T => T.cityName))
+                      .ForMember(pd => pd.CountryId, o => o.MapFrom(T => T.countryId))
+                       .ForMember(dest => dest.Country, opt => opt.MapFrom(src => new Country
+                       {
+                           NameCountry = src.countryName,
+                           Contient = src.contient
+                       }))
+                .ReverseMap();
+
+
+        }
+    }
+   
+}
