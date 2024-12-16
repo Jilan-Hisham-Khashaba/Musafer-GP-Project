8293a0717b72f6cc8f332cb01622b4f4da5c9a15
Jilan-Hisham-Khashaba <154570411+Jilan-Hisham-Khashaba@users.noreply.github.com>
2024-12-16 04:08:39 +0200

    Add files via upload

---
+++ Back end/Gp.Api solution/AdminDashBoard/Middelwares/TokenValidationMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using GP.core.Entities.identity;
+using GP.core.Services;
+using GP.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminDashBoard.Middelwares
+{
+    //    public class TokenValidationMiddleware
+    //    {
+    //        private readonly RequestDelegate _next;
+
+    //        public TokenValidationMiddleware(RequestDelegate next)
+    //        {
+    //            _next = next;
+    //        }
+
+    //        public async Task Invoke(HttpContext context)
+    //        {
+    //            var token = context.Request.Cookies["token"]; // الحصول على التوكن من الـ cookies
+    //            if (!string.IsNullOrEmpty(token))
+    //            {
+    //                // تأكد من إزالة أي توكن قديم موجود في الرأس
+    //                if (context.Request.Headers.ContainsKey("Authorization"))
+    //                {
+    //                    context.Request.Headers.Remove("Authorization");
+    //                }
+
+    //                // إضافة التوكن إلى الرأس
+    //                context.Request.Headers.Add("Authorization", "Bearer " + token);
+    //            }
+
+    //            await _next(context);
+    //        }
+    //    }
+}
+//}
+
+
+
+
