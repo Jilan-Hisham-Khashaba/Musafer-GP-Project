b5279b5d96fccfa9937434a9d56057aa49b13cf1
Jilan-Hisham-Khashaba <154570411+Jilan-Hisham-Khashaba@users.noreply.github.com>
2024-12-17 23:51:28 +0200

    Add files via upload

---
+++ Back end/Gp.Api solution/AdminPanal/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using AdminPanal.Models;
+using Emgu.CV.Ocl;
+using GP.core.Entities.identity;
+using GP.Core.Entities;
+using GP.Core.Repositories;
+using GP.Repository.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPanal.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly StoreContext context;
+        private readonly IGenericRepositroy<Shipment> shipmentRepo;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<AppUser> userManager,StoreContext context,IGenericRepositroy<Shipment> shipmentRepo ,RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.shipmentRepo = shipmentRepo;
+            this.roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.Select(u=>new UserView
+            {
+                Id = u.Id,
+                UserName=u.UserName,
+                DisplayName=u.DisplayName,
+                Email =u.Email,
+                PhoneNumber=u.PhoneNumber,
+                //ShipmentCount = context.shipments.Count(st => st.IdentityUserId == u.Id ),
+                //TripCount = context.Trips.Count(st => st.UserId == u.Id )
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
---
+++ Back end/Gp.Api solution/AdminPanal/Models/UserView.cs
@@ -0,0 +1,21 @@
+﻿namespace AdminPanal.Models
+{
+    public class UserView
+    {
+        public string Id { get; set; }
+
+        public string  UserName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int ShipmentCount { get; set;}
+
+        public int TripCount { get; set; }
+
+
+    }
+}
